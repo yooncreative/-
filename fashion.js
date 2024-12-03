051dc3b4baacbece534e05a39586c27a98b3a020
yooncreative <yooncreative96@naver.com>
2024-12-03 15:17:49 +0900

    Update fashion.js

--- fashion.js
+++ fashion.js
@@ -1,15 +1,2 @@
 const video = document.getElementById('playlistVideo');
 
-window.addEventListener('scroll', () => {
-  const scrollY = window.scrollY;
-  const scrollableHeight = document.body.scrollHeight - window.innerHeight;
-  const scrollFraction = scrollY / scrollableHeight;
-
-  // 스크롤에 따른 비디오 시간 설정
-  video.currentTime = scrollFraction * video.duration;
-
-  // 스크롤 방향에 따른 재생 속도 설정
-  const playbackRate = scrollY > video.currentTime ? 1 : -1;
-  video.playbackRate = playbackRate;
-  video.play();
-});
